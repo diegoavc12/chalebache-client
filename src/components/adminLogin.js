6ea1d311e7cc9fb1061bab2d1007e8582cee5e5c
Daniel Ortiz <dortizjoachin@gmail.com>
2021-06-08 12:50:35 -0500

    change env variables

--- src/components/adminLogin.js
+++ src/components/adminLogin.js
@@ -13,6 +13,7 @@ const AdminLogin =  () =>  {
 
         const sendLogin = async() =>{
             const API_CRUD="http://localhost:3030"
+            console.log(process.env.API_CRUD);
             const resp = await axios.post(`${process.env.API_CRUD}/admin/login`,
             {
                 "username": data.username, "password":data.password
